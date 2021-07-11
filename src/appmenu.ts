1368fac2b4b9008095d66dbfdcb3e0b25fa7a20f
Guczi Ádám <gucziadamlaszlo@gmail.com>
2021-07-11 15:53:24 +0200

    Toggle developer tools menu

--- src/appmenu.ts
+++ src/appmenu.ts
@@ -1,16 +1,23 @@
 import { Menu } from "electron"
 import { ImportSheet } from "./sheet-io"
 
-const template = [
+const template: Electron.MenuItemConstructorOptions[] = [
     {
         label: "File",
         submenu: [{
             label: "Import",
             click: ImportSheet
         }]
+    },
+    {
+        label: "Developer",
+        submenu: [{
+            label: "Toggle Developer Tools",
+            accelerator: "CmdOrCtrl+Shift+I",
+            role: "toggleDevTools"
+        }]
     }
 ]
-
 
 const menu = Menu.buildFromTemplate(template)
 Menu.setApplicationMenu(menu)
