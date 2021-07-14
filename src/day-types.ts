d2ef51a9f595c26393969d919a9f4933076b39f0
frsaba <frsaba.dev@gmail.com>
2021-07-14 09:11:25 +0200

    Font color contrast (manual)

--- src/day-types.ts
+++ src/day-types.ts
@@ -21,12 +21,12 @@ interface DayTypeDescription {
 let theme = Vuetify.userPreset.theme!.themes!.light!
 
 export const DayTypeDescriptions = [
-    { type: "shift", 	color: theme.shift,   desc: "Műszak",          label: "M"},
-    { type: "paid", 	color: theme.paid, desc: "Fizetett szabadság", label: "FSZ"  },
-    { type: "unpaid", 	color: theme.unpaid, desc: "Szabadnap", 	    label: "SZ/P" },
-    { type: "holiday", 	color: theme.holiday,  desc: "Fizetett ünnep", 	  label: "FÜ"   },
-    { type: "weekend", 	color: theme.free_weekend, desc: "Szabad hétvége", 	  label: "SZH"  },
-    { type: "rest", 	color: theme.rest, desc: "Pihenőnap", 	      label: "*"    },
-    { type: "sick", 	color: theme.sick, 	  desc: "Táppénz", 			  label: "TP"   },
-    { type: "empty", 	color: theme.empty,   desc: "Üres", 			  label: "-"    }
+    { type: "shift", 	color: theme.shift,         desc: "Műszak",             label: "M"},
+    { type: "paid", 	color: theme.paid,          desc: "Fizetett szabadság", label: "FSZ"  },
+    { type: "unpaid", 	color: theme.unpaid,        desc: "Szabadnap", 	        label: "SZ/P" },
+    { type: "holiday", 	color: theme.holiday,       desc: "Fizetett ünnep",     label: "FÜ"   },
+    { type: "weekend", 	color: theme.free_weekend,  desc: "Szabad hétvége",     label: "SZH"  },
+    { type: "rest", 	color: theme.rest,          desc: "Pihenőnap", 	        label: "*"    },
+    { type: "sick", 	color: theme.sick, 	        desc: "Táppénz", 			label: "TP"   },
+    { type: "empty", 	color: theme.empty,         desc: "Üres", 			    label: "-"    }
 ] as DayTypeDescription[]
