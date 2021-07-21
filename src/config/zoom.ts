cd0333f7f43dc08e154868076b0a587ffb2cb37a
frsaba <frsaba.dev@gmail.com>
2021-07-21 16:40:41 +0200

    Use clamp in zoom.ts

--- src/config/zoom.ts
+++ src/config/zoom.ts
@@ -1,4 +1,5 @@
 import { BrowserWindow } from "electron";
+import { clamp } from "lodash";
 let [min, max] = [0.5, 2]; // 50% - 200%
 let changeFactor = 0.1;
 
@@ -9,7 +10,7 @@ export default function init(win: BrowserWindow)
         let zoom = win.webContents.getZoomFactor();
         
         zoom += (zoomDirection === "in" ? changeFactor : -changeFactor);
-        zoom = Math.min(Math.max(zoom, min), max);
+        zoom = clamp(zoom, min, max)
         win.webContents.setZoomFactor(zoom);
 
         win.webContents.send("zoom", {zoom});
