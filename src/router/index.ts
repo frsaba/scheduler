fb13181875e53fbd6b904f48c7885e4f0a484156
frsaba <frsaba.dev@gmail.com>
2021-07-29 15:42:20 +0200

    Split panes proof of concept

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,15 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import Home from "@/views/Home.vue";
-import Monthly from "@/views/Monthly.vue"
+import Editor from "@/views/Editor.vue"
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
     path: "/",
-    name: "Monthly",
-    component: Monthly,
+    name: "Editor",
+    component: Editor,
   },
   {
     path: "/home",
---
+++ src/views/Editor.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import Vue from 'vue'
+import { Splitpanes, Pane } from 'splitpanes'
+import 'splitpanes/dist/splitpanes.css'
+import Monthly from "@/views/Monthly.vue"
+export default Vue.extend({
+	name: "Editor",
+	components: { Splitpanes, Pane, Monthly },
+})
+</script>
+
+<template>
+	<splitpanes class="default-theme">
+		<pane min-size="50">
+			<monthly />
+		</pane>
+		<pane>
+
+			<splitpanes class="default-theme" horizontal>
+				<pane>
+					<div>Warnings and errors</div>
+				</pane>
+				<pane>
+					<div>Global aggregates</div>
+				</pane>
+			</splitpanes>
+		</pane>
+
+	</splitpanes>
+</template>
