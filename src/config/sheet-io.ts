57b1f493d1226f711dbe327ed7875f23d34d9985
Guczi Ádám <gucziadamlaszlo@gmail.com>
2021-08-07 18:09:58 +0200

    Parse template cells as JSON

--- src/config/sheet-io.ts
+++ src/config/sheet-io.ts
@@ -1,102 +1,122 @@
 import { dialog, BrowserWindow, ipcMain } from "electron"
-import * as Excel from 'exceljs';
+import Excel from 'exceljs';
 import { Sheet } from "@/model/schedule-sheet";
-import _, { bind } from "lodash";
-import strftime from "strftime"
 import Path from "path"
 
 let sheet: Sheet;
-let bindings: Map<string, Function>
+let bindings: Map<string, (...args: any[]) => Excel.CellValue>;
 
 export function ImportSheet() {
-    const path = GetPathFromUser();
-    if (path) {
-        ReadFile(path);
-    }
+	const path = GetPathFromUser();
+	if (path) {
+		ReadFile(path);
+	}
 }
 
 export function ExportSheet() {
-    let window = BrowserWindow.getFocusedWindow()!;
-    window.webContents.send("export-query");
-    // const path = GetPathFromUser();
-    // if (path) {
-    //     // writeFile(path);
-    // }
+	let window = BrowserWindow.getFocusedWindow()!;
+	window.webContents.send("export-query");
 }
 
 ipcMain.on("export-reply", async (event: Event, s: Sheet) => {
-    sheet = s;
-    bindings = new Map<string, () => string>([
-        ["year", () => sheet.year.toString()],
-        ["month", () => sheet.month.toString()],
-        ["startDate", () => strftime("%F", new Date(sheet.year, sheet.month, 1))]
-    ])
-    let templatePath = Path.join(__dirname, "../src/assets/template.xlsx");
-    let exportPath = Path.join(__dirname, "../src/assets/out.xlsx");
-    await writeFile(templatePath, exportPath);
+	sheet = s;
+	bindings = new Map<string, (...args: any[]) => Excel.CellValue>([
+		["year", () => sheet.year],
+		["month", () => sheet.month],
+		["startDate", () => {
+			return new Date(sheet.year, sheet.month, 1)
+		}],
+		["startsAt", (hour: number) => {
+			return hour
+		}]
+	])
+	let templatePath = Path.join(__dirname, "../src/assets/template.xlsx");
+	let exportPath = Path.join(__dirname, "../src/assets/out.xlsx");
+	await writeFile(templatePath, exportPath);
 })
 
 export async function writeFile(templatePath: string, exportPath: string) {
-    let workbook = new Excel.Workbook();
-    let template = (await workbook.xlsx.readFile(templatePath)).worksheets[0];
+	let workbook = new Excel.Workbook();
+	await workbook.xlsx.readFile(templatePath)
+	let template = workbook.getWorksheet("Main");
 
-    template.eachRow({ includeEmpty: true }, (row, i) => {      
-        row.eachCell({ includeEmpty: true }, (current, j) => {
-            current.value = replaceTemplate(current.value);
-        });
-    })
+	template.eachRow({ includeEmpty: true }, (row, i) => {
+		row.eachCell({ includeEmpty: true }, (current, j) => {
+			current.value = replaceTemplate(current);
+		});
+	})
 
-    // let copyTo = template.getCell("");
-    // copyTo.value = template.getCell("A12").value;
-    
-    await workbook.xlsx.writeFile(exportPath);
+	await workbook.xlsx.writeFile(exportPath)
 }
 
-let reg = /(?<=\$\{).*?(?=\})/g;
+let reg = /(?<=\$)\{.*?\}/g;
 
-function replaceTemplate(str: Excel.CellValue) {
-    if (!str || typeof str !== "string") return str;
+function replaceTemplate(cell: Excel.Cell) {
+	if (!cell.value || typeof cell.value !== "string") return cell.value;
 
-    let matches = Array.from(str.matchAll(reg));
+	let matches = Array.from(cell.value.matchAll(reg)).flat();
+	let result: Excel.CellValue = cell.value;
 
-    for (let value of matches.flat()) {
-        let fn = bindings.get(value)
-        if (fn)
-            str = str.replace(`\${${value}}`, fn());
-    }
+	for (const match of matches) {
+		let obj: { [key: string]: any[] } = JSON.parse(jsonify(match));
+		if (!obj) {
+			console.error(`Couldn't parse ${match}`);
+			continue;
+		}
+		for (const key in obj) {
+			let func = bindings.get(key);
+			if (func) {
+				let retVal = func(...obj[key]);
+				if (!retVal) retVal = ""
 
-    return str
+				// If the template is the only thing in the string keep the return type
+				if (cell.value.length === match.length + 1)
+					result = retVal;
+				// If there are other things convert it to a string
+				else
+					result = result!.toString().replace(`$${match}`, retVal.toString())
+			}
+		}
+	}
+
+	return result
 }
 
-function containsBindings(cell: string, ...bindings: string[]) {
-    if (!cell) return false;
-    
-    let matches = Array.from(cell.matchAll(reg));
+// Corrects the incoming string so that it can be parsed by JSON
+function jsonify(str: string) {
+	if (!str) return "{}";
 
-    return matches.flat().some(x => bindings.includes(x))   
+	// Trim leading and trailing curly braces
+	str = str.replace(/^\{?|\}?$/g, "");
+
+	let [key, value] = str.split(":", 2);
+	if (!key) return "{}";
+	// Trim leading and trailing quotation marks
+	key = key.replace(/^[\"\']?|[\"\']?$/g, "");
+
+	if (value) return `{"${key}": ${value}}`;
+	else return `{"${key}": []}`;
 }
 
 function GetPathFromUser(): string | undefined {
-    var result = dialog.showOpenDialogSync({
-        properties: ['openFile'],
-        filters: [
-            { name: 'Excel files', extensions: ['xlsx', 'xls'] },
-        ]
-    });
-    if (result) {
-        var path = result[0];
+	var result = dialog.showOpenDialogSync({
+		properties: ['openFile'],
+		filters: [
+			{ name: 'Excel files', extensions: ['xlsx', 'xls'] },
+		]
+	});
+	if (result) {
+		var path = result[0];
 
-        return path
-    }
-    return undefined;
+		return path
+	}
+	// @ts-ignore
+	const a = CountStartingTimes(undefined);
+	return undefined;
 }
 
 async function ReadFile(path: string) {
-    const workbook = new Excel.Workbook();
-    return await workbook.xlsx.readFile(path);
-    console.log(workbook.worksheets[0])
+	const workbook = new Excel.Workbook();
+	return await workbook.xlsx.readFile(path);
+	console.log(workbook.worksheets[0])
 }
-
-// function ParseSheet() : Sheet{
-
-// }
