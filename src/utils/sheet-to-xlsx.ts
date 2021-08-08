467fbe025d8bc866697f11a0f6745c09a236b1da
Guczi Ádám <gucziadamlaszlo@gmail.com>
2021-08-08 15:39:55 +0200

    Excel copy employee rows

--- src/utils/sheet-to-xlsx.ts
+++ src/utils/sheet-to-xlsx.ts
@@ -2,14 +2,42 @@ import Excel from 'exceljs';
 import { Sheet } from "@/model/schedule-sheet";
 import _ from "lodash"
 import { CountStartingTimes } from "@/model/aggregates"
+import { DayType, DayTypeDescriptions } from '@/model/day-types';
 
 type BindingMap = Map<string, (...args: any[]) => Excel.CellValue>
+class Position {
+	row: number
+	col: number
+	constructor(row: number | string, col: number | string) {
+		this.row = Number(row)
+		this.col = Number(col)
+	}
+
+	subtract(other: Position) {
+		return new Position(this.row - other.row, this.col - other.col)
+	}
+}
+
+function copyRange(worksheet: Excel.Worksheet, start: Position, end: Position, target: Position) {
+	let offset = target.subtract(start)
+	for (let row = start.row; row <= end.row; row++) {
+		for (let col = start.col; col <= end.col; col++) {
+			let sourceCell = worksheet.getCell(row, col)
+			let targetCell = worksheet.getCell(row + offset.row, col + offset.col)
+			targetCell.value = sourceCell.value
+		}
+	}
+}
 
 export async function bufferToWorkbook(buffer: Excel.Buffer) {
 	return await new Excel.Workbook().xlsx.load(buffer)
 }
 
 export default async function replaceTemplate(template: Excel.Worksheet, sheet: Sheet) {
+	let employeeIndex: Position
+	let employeeDistance: Position
+	let currentScheduleRow: number
+
 	let bindings: BindingMap = new Map<string, (...args: any[]) => Excel.CellValue>([
 		["year", () => sheet.year],
 		["month", () => sheet.month],
@@ -17,18 +45,72 @@ export default async function replaceTemplate(template: Excel.Worksheet, sheet: 
 			return new Date(sheet.year, sheet.month, 1)
 		}],
 		["startsAt", (cell: Excel.Cell, hour: number) => {
-			let day = Number(cell.col) - 2; // Assuming days start at column B
-			return CountStartingTimes(sheet).get(hour)?.[day] ?? 0
-		}]
+			let day = Number(cell.col) - 1; // Assuming days start at column B
+			return CountStartingTimes(sheet).get(hour)?.[day - 1] ?? 0
+		}],
+		["employeeName", (cell: Excel.Cell) => {
+			let current = new Position(cell.row, cell.col)
+			if (!employeeIndex) employeeIndex = current
+
+			currentScheduleRow = 0
+			return sheet.GetRow(currentScheduleRow).employee.name
+		}],
+		["nextEmployee", (cell: Excel.Cell) => {
+			let current = new Position(cell.row, cell.col)
+			if (!employeeDistance)
+				employeeDistance = current.subtract(employeeIndex)
+
+			currentScheduleRow = (current.row - employeeIndex.row) / employeeDistance.row;
+			// Only copy to next row if there is a next employee
+			if (currentScheduleRow < sheet.schedule.length - 1) {
+				let next = new Position(current.row + employeeDistance.row, current.col + employeeDistance.col)
+				copyRange(template, current, new Position(current.row + employeeDistance.row - 1, 50), next)
+			} else {
+				// Add conditional formatting for weekends on the last employee
+				template.addConditionalFormatting({
+					ref: `B$${employeeIndex.row - 1}:AF${current.row + employeeDistance.row - 1}`,
+					rules: [
+						{
+							priority: 1,
+							type: "expression",
+							formulae: [`WEEKDAY(DATE(${sheet.year},${sheet.month},B$${employeeIndex.row - 1}),2)>5`],
+							style: { fill: { type: "pattern", pattern: "solid", bgColor: { argb: "FFFFFF00" } } },
+						}
+					]
+				})
+			}
+			return sheet.GetRow(currentScheduleRow).employee.name
+		}],
+		["shiftStart", (cell: Excel.Cell) => {
+			let current = new Position(cell.row, cell.col)
+
+			let day = current.col - 1; // Assuming days start at column B
+			if (day > sheet.month_length) return ""
+
+			let scheduleDay = sheet.GetRow(currentScheduleRow).GetDay(day)
+			if (scheduleDay.type == DayType.shift)
+				return scheduleDay.start
+			else
+				return DayTypeDescriptions[scheduleDay.type].label
+		}],
+		["shiftEnd", (cell: Excel.Cell) => {
+			let day = Number(cell.col) - 1; // Assuming days start at column B
+			if (day > sheet.month_length) return ""
+			let scheduleDay = sheet.GetRow(currentScheduleRow).GetDay(day)
+			if (scheduleDay.type == DayType.shift)
+				return (scheduleDay.start + scheduleDay.duration) % 24
+			else
+				return ""
+		}],
 	])
 
-	template.eachRow({ includeEmpty: true }, (row, i) => {
-		row.eachCell({ includeEmpty: true }, (current, j) => {
+	template.eachRow((row, i) => {
+		row.eachCell((current, j) => {
 			current.value = replaceTemplateCell(current, bindings);
-
 			// In the template the fomula's result is an error so the output file will be broken without this
 			if (current.formula)
 				current.value = ({ formula: current.formula, result: 0 } as Excel.CellFormulaValue)
+
 		});
 	})
 }
